6040d52d406c49721171e160cc9986b2cb47a3b6
dhara519 <dhara519@gmail.com>
2024-06-13 16:10:12 -0400

    commiting html and js of assignment

---
+++ Secure_the_Vault.js
@@ -0,0 +1,15 @@
+// Declare 3 different variables.
+// Assign each variable an arguement that equates to '10','40',and '39' respectively.
+// Ensure each arguement is using a unique arithmetic operator.
+let x = 100 / 10;
+let y = 20 * 2;
+let z = 30 + 9;
+
+// Alert the user their golden opportunity of a lifetime to open an important vault.
+// Refer to each variable using curly brackets and backtiks to ensure the message will display the calculated result of each variable
+
+alert(
+  `You have received this message because you have been chosen to open an important vault. Here is the secret combination: ${x} ${y} ${z}`
+);
+
+// yay (:
